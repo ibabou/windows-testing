774baebfeee400b63d833869f5cc479b624f014f
“Ibrahim <ibrahimab@google.com>
2022-04-27 07:59:20 +0000

    testing

--- gce/run-e2e.sh
+++ gce/run-e2e.sh
@@ -15,9 +15,9 @@ sleep ${INIT_TIMEOUT:-1s}
 # Taint the Linux nodes to prevent the test workloads from landing on them.
 # TODO: remove this once the issue is resolved:
 # https://github.com/kubernetes/kubernetes/issues/69892
+LINUX_NODES=$(kubectl get nodes -l beta.kubernetes.io/os=linux -o name)
+LINUX_NODE_COUNT=$(echo ${LINUX_NODES} | wc -w)
 if [[ ! -v NO_LINUX_POOL_TAINT ]]; then
-  LINUX_NODES=$(kubectl get nodes -l beta.kubernetes.io/os=linux -o name)
-  LINUX_NODE_COUNT=$(echo ${LINUX_NODES} | wc -w)
   for node in $LINUX_NODES; do
     kubectl taint node $node node-under-test=false:NoSchedule
   done
@@ -50,12 +50,6 @@ if [[ -v PREPULL_YAML && ! -z "$PREPULL_YAML" ]]; then
   # Wait a few more minutes for the pod to be cleaned up.
   timeout 3m kubectl wait --for=delete pod -l prepull-test-images=e2e --timeout -1s
 fi
-
-# Download and set the list of test image repositories to use.
-curl \
-  ${KUBE_TEST_REPO_LIST_DOWNLOAD_LOCATION:-https://raw.githubusercontent.com/kubernetes-sigs/windows-testing/master/images/image-repo-list} \
-  -o ${WORKSPACE}/repo-list.yaml
-export KUBE_TEST_REPO_LIST=${WORKSPACE}/repo-list.yaml
 
 # When using customized test command (which we are now), report-dir is not set
 # by default, so set it here.
